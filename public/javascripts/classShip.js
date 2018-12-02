1b064333b178eea4c05c1902a02955fcd5df3ecf
P. te Marvelde <ptemarvelde@gmail.com>
2018-12-02 16:09:39 +0100

    a

---
+++ public/javascripts/classShip.js
@@ -0,0 +1,11 @@
+class Ship {
+    /*  length = lenghth of the ship
+        start  = starting coordinate
+        end    = ending coordinate
+        sunk   = ship "sunk" or not (boolean)
+    */
+
+    constructor(length, start, end, sunk){
+
+    }
+}
