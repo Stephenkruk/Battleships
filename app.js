ad842d1df240d438e7fbc6f630b2a524c56d842d
Stephenkruk <45105031+Stephenkruk@users.noreply.github.com>
2018-12-07 23:20:57 +0100

    server

--- app.js
+++ app.js
@@ -1,16 +1,24 @@
 var express = require("express");
 var http = require("http");
+var websocket = require("ws");
 
 var indexRouter = require("./routes/index");
 
 var port = 3000;
 var app = express();
 
-app.use(express.static(__dirname + "/public"));
-http.createServer(app).listen(port);
-
 app.get("/play", indexRouter);
 
 app.get("/", (req, res) => {
     res.sendFile("splash.html", {root: "./public"});
 });
+
+app.use(express.static(__dirname + "/public"));
+var server = http.createServer(app);
+
+const wss = new WebSocket.Server({ server });
+
+wss.on("connection", function() {
+
+});
+server.listen(port);
---
+++ game.js
@@ -0,0 +1,9 @@
+function Game(id, grid1, ship1, grid2, ship2) {
+    this.player1 = null;
+    this.player2 = null;
+    this.id = id;
+    this.grid1 = grid1;
+    this.ship1 = ship1;
+    this.grid2 = grid2;
+    this.ship2 = ship2;
+}
