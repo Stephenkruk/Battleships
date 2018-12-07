0e3cf6fa1480b332859df67de8bfca9ffb07151d
Stephenkruk <45105031+Stephenkruk@users.noreply.github.com>
2018-12-07 19:26:54 +0100

    server related code

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var express = require("express");
 var http = require("http");
 
+var indexRouter = require("./routes/index");
+
 var port = process.argv[2];
 var app = express();
 
 app.use(express.static(__dirname + "/public"));
 http.createServer(app).listen(port);
+
+app.get("/play", indexRouter);
+
+app.get("/", (req, res) => {
+    res.render("splash.html");
+});
